0d0cadbe0a1e98b065e81921a4e0b584dd53c30b
Munhsarnai <saak0414@gmail.com>
2026-09-15 12:11:03 +0800

    Add push and pop functions to Queue
    Implemented q_push and q_pop functions for Queue.

--- queue.c
+++ queue.c
@@ -1,23 +1,24 @@
 #include "DS.h"
 
-/* p-ийн зааж буй Queue-д x утгыг хийнэ */
-void q_push(Queue *p, int x)
-{
-        /* Энд оруулах үйлдлийг хийнэ үү */
+/*
+ * Тайлбар: доорх функцүүд нь Queue бүтцийн p->q_arr[] хүснэгт болон
+ * p->q_len хувьсагчтай харьцана.
+ */
+
+// Төгсгөлд нь оруулах үйлдэл. p-ийн зааж буй Queue-д x утгыг хийнэ.
+void q_push(Queue *p, int x) {
+    p->q_arr[p->q_len] = x;
+    p->q_len++;
 }
 
-/* p-ийн зааж буй Queue-с гаргана */
-void q_pop(Queue *p)
-{
-        /* Энд гаргах үйлдлийг хийнэ үү */
-}
-
-/* p-ийн зааж буй Queue-н утгуудыг хэвлэнэ */
-void q_print(Queue *p)
-{
-        int i;
-        for (i = 0; i < p->q_len; i++) {
-                printf("%d ", p->q_arr[i]);
-        }
-        printf("\n");
+// Эхлэлээс нь гаргах үйлдэл. p-ийн зааж буй Queue-с гаргана.
+// p->q_arr хүснэгтийн элементүүд нэг нэг байрлал урагшилна.
+void q_pop(Queue *p) {
+    if (p->q_len == 0) {
+        return;
+    }
+    for (int i = 0; i < p->q_len - 1; i++) {
+        p->q_arr[i] = p->q_arr[i + 1];
+    }
+    p->q_len--;
 }
